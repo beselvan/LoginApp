c3dc2b00d853d682fea4eda15157eb2d6ddb11b2
beselvan <43469566+beselvan@users.noreply.github.com>
2020-06-06 14:19:09 +0530

    Update db_script.sql

--- database/db_script.sql
+++ database/db_script.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE DEMO;
 USE DEMO;
-CREATE TABLE USER (first_name VARCHAR(30), last_name VARCHAR(30), email VARCHAR(30), username VARCHAR(30), password VARCHAR(30), regdate DATE);
+CREATE TABLE user (first_name VARCHAR(30), last_name VARCHAR(30), email VARCHAR(30), username VARCHAR(30), password VARCHAR(30), regdate DATE);
 
-insert into USER(first_name, last_name, email, username, password, regdate) values ('test','test','test@test.com','test','test', CURDATE());
+insert into user(first_name, last_name, email, username, password, regdate) values ('test','test','test@test.com','test','test', CURDATE());
