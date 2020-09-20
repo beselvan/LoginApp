da05b57d4414218782247a5f118ebc8e019832e9
beselvan <43469566+beselvan@users.noreply.github.com>
2020-09-20 19:00:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,19 @@
 # Linux
 yum update -y
 yum upgrade -y
-yum install -y git zip unzip tar net-tools ntsysv java-1.8.0-openjdk.x86_64 maven wget
+yum install -y git zip unzip tar net-tools ntsysv java-1.8.0-openjdk.x86_64 maven wget curl
 #------------------------------------------------------------------------------------------
 # Docker 
 
-wget https://download.docker.com/linux/centos/docker-ce.repo -P /etc/yum.repos.d/
-chmod 644 /etc/yum.repos.d/docker-ce.repo
-groupadd docker
-yum install -y docker docker-compose.noarch
-systemctl[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg start docker
+sudo yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+yum install docker-ce docker-ce-cli -y
+service docker start
 systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod 755 /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 #------------------------------------------------------------------------------------------
 # Jenkins
